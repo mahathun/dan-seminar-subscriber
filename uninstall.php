ac0f4131603dfac8770a0635e3dda95b935a14b2
Dan Mahavithana <mahathun.online@gmail.com>
2017-01-30 09:07:00 +1300

    fixed the fatal error by commenting out uninstallation code

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,4 @@
 <?php
-if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
-    global $wpdb;
-    $wpdb->query( "DROP TABLE IF EXISTS wp_dan_seminar_register" );
+// if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
+//     global $wpdb;
+//     $wpdb->query( "DROP TABLE IF EXISTS wp_dan_seminar_register" );
